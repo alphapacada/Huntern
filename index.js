aadb2c7c0a8f76d64ff1bcfb704018c12a2f44a5
arpacada <arpacada@up.edu.ph>
2017-05-27 22:33:59 +0800

    search with filter

--- index.js
+++ index.js
@@ -37,107 +37,107 @@ app.use(passport.initialize());
 
 app.get('/', function(req, res) {
 	console.log('body of render.');
-	res.render('index.html');
+	res.render('filter.html');
 
 });
  // signIn middleware
 
-var retrieveSignedInUser = function(req, res, next) {
-	const email = req.session.currentUser;
+// var retrieveSignedInUser = function(req, res, next) {
+// 	const email = req.session.currentUser;
 
-    User.findOne({ where: { email:email } }).then(function(user) {
-    	console.log("retrieveSignedInUser" + user);
+//     User.findOne({ where: { email:email } }).then(function(user) {
+//     	console.log("retrieveSignedInUser" + user);
     	
-    	console.log("retrieveSignedInUser2" + req.session.currentUser);
-    	console.log("retrieveSignedInUser3" + req.session.currentUser);
-    	req.user = user;
-    	next();
-    });
-}
-app.get('/profile', requireSignedIn, retrieveSignedInUser, function(req, res) {
-	const email = req.user;
-	const name = req.user;
-	console.log("PROFILE" + email);
-		res.render('profile.html', {
-			user: req.user
-		}); 
-});
+//     	console.log("retrieveSignedInUser2" + req.session.currentUser);
+//     	console.log("retrieveSignedInUser3" + req.session.currentUser);
+//     	req.user = user;
+//     	next();
+//     });
+// }
+// app.get('/profile', requireSignedIn, retrieveSignedInUser, function(req, res) {
+// 	const email = req.user;
+// 	const name = req.user;
+// 	console.log("PROFILE" + email);
+// 		res.render('profile.html', {
+// 			user: req.user
+// 		}); 
+// });
 
-app.post('/transfer', requireSignedIn, retrieveSignedInUser, function(req, res) {
-	//console.log("transfer" + req.user);
-	const recipient = req.body.recipient;
-	const amount = parseInt(req.body.amount, 10);
-	const user = req.user;
-	console.log("TRANSFER" + user.id);
-	const email = req.session.currentUser;
-	const id = user.id;
+// app.post('/transfer', requireSignedIn, retrieveSignedInUser, function(req, res) {
+// 	//console.log("transfer" + req.user);
+// 	const recipient = req.body.recipient;
+// 	const amount = parseInt(req.body.amount, 10);
+// 	const user = req.user;
+// 	console.log("TRANSFER" + user.id);
+// 	const email = req.session.currentUser;
+// 	const id = user.id;
 	
-	User.findOne({where:{email:recipient}}).then(function(senderAccount) {
-			Account.findOne({ where: { user_id: id, user_id:senderAccount.id}}).then(function(receiverAccount) {
+// 	User.findOne({where:{email:recipient}}).then(function(senderAccount) {
+// 			Account.findOne({ where: { user_id: id, user_id:senderAccount.id}}).then(function(receiverAccount) {
 		
-			console.log("RECEIVER" + Account);
-				// Account.findOne({ where: { user_id: receiver.id } }).then(function(receiverAccount) {
-					database.transaction(function(t) {
-						return senderAccount.update({
-							balance: senderAccount.balance - amount
-						}, { transaction: t }).then(function() {
-							return receiverAccount.update({
-								balance: receiverAccount.balance + amount
-							}, { transaction: t });
-						});
-					}).then(function() {
-						req.flash('statusMessage', 'Transferred ' + amount + ' to ' + recipient);
-						res.redirect('/profile');
-					});
-	});
+// 			console.log("RECEIVER" + Account);
+// 				// Account.findOne({ where: { user_id: receiver.id } }).then(function(receiverAccount) {
+// 					database.transaction(function(t) {
+// 						return senderAccount.update({
+// 							balance: senderAccount.balance - amount
+// 						}, { transaction: t }).then(function() {
+// 							return receiverAccount.update({
+// 								balance: receiverAccount.balance + amount
+// 							}, { transaction: t });
+// 						});
+// 					}).then(function() {
+// 						req.flash('statusMessage', 'Transferred ' + amount + ' to ' + recipient);
+// 						res.redirect('/profile');
+// 					});
+// 	});
 	
 	 
-});
+// });
 
-});
-app.post('/deposit', requireSignedIn,  retrieveSignedInUser, function(req, res){
-	const amount = parseInt(req.body.amount, 10);
-	const sender =  req.user.id;
-	console.log("DEPOSIT");
+// });
+// app.post('/deposit', requireSignedIn,  retrieveSignedInUser, function(req, res){
+// 	const amount = parseInt(req.body.amount, 10);
+// 	const sender =  req.user.id;
+// 	console.log("DEPOSIT");
 
 	
-		Account.findOne({ where: { user_id: sender } }).then(function(senderAccount) {
-			database.transaction(function(t) {
-				return senderAccount.update({
-					balance: senderAccount.balance + amount
-				}, { transaction: t });
-			}).then(function() {
-				req.flash('statusMessage', 'Deposit of ' + amount + ' is succesful');
-				res.redirect('/profile');
-			});
-		});
-	});
+// 		Account.findOne({ where: { user_id: sender } }).then(function(senderAccount) {
+// 			database.transaction(function(t) {
+// 				return senderAccount.update({
+// 					balance: senderAccount.balance + amount
+// 				}, { transaction: t });
+// 			}).then(function() {
+// 				req.flash('statusMessage', 'Deposit of ' + amount + ' is succesful');
+// 				res.redirect('/profile');
+// 			});
+// 		});
+// 	});
 
 
-app.post('/withdraw', requireSignedIn,retrieveSignedInUser, function(req, res){
-	const amount = parseInt(req.body.amount, 10);
-	const sender = req.user.id
+// app.post('/withdraw', requireSignedIn,retrieveSignedInUser, function(req, res){
+// 	const amount = parseInt(req.body.amount, 10);
+// 	const sender = req.user.id
 
-	//User.findOne({ where: { email: email } }).then(function(sender) {
-		Account.findOne({ where: { user_id: sender } }).then(function(senderAccount) {
-			database.transaction(function(t) {
-				return senderAccount.update({
-					balance: senderAccount.balance - amount
-				}, { transaction: t });
-			}).then(function() {
-				req.flash('statusMessage', 'Withdrawal of ' + amount + ' is succesful');
-				res.redirect('/profile');
-			});
-		});
+// 	//User.findOne({ where: { email: email } }).then(function(sender) {
+// 		Account.findOne({ where: { user_id: sender } }).then(function(senderAccount) {
+// 			database.transaction(function(t) {
+// 				return senderAccount.update({
+// 					balance: senderAccount.balance - amount
+// 				}, { transaction: t });
+// 			}).then(function() {
+// 				req.flash('statusMessage', 'Withdrawal of ' + amount + ' is succesful');
+// 				res.redirect('/profile');
+// 			});
+// 		});
 	
-});
+// });
 
-function requireSignedIn(req, res, next) {
-    if (!req.session.currentUser) {
-        return res.redirect('/');
-    }
-    next();
-}
+// function requireSignedIn(req, res, next) {
+//     if (!req.session.currentUser) {
+//         return res.redirect('/');
+//     }
+//     next();
+// }
 
 app.listen(3000, function() {
 	console.log('Server is now running at port 3000');
